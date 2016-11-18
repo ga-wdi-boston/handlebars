ad46a0b291d729e1477c631d7fd187ffde025a9c
payne-chris-r <06cpayne@gmail.com>
2016-11-18 14:35:12 -0500

    Remove ui success stuff.

--- assets/scripts/books/ui.js
+++ assets/scripts/books/ui.js
@@ -3,13 +3,11 @@
 const showBooksTemplate = require('../templates/book-listing.handlebars');
 
 const getBooksSuccess = (books) => {
-  $('#content').append(showBooksTemplate(books));
 
-  console.log(books);
 };
 
 const clearBooks = () => {
-  $('#content').html("");
+  
 };
 
 const failure = (error) => {
