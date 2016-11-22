9b1aae9bce449607d868e38b88f816590af5d28d
payne-chris-r <06cpayne@gmail.com>
2016-11-22 14:25:03 -0500

    Left off -id from my new DOM attr.

--- assets/scripts/books/api.js
+++ assets/scripts/books/api.js
@@ -4,7 +4,8 @@ const app = require('../app.js');
 
 let getBooks = function(){
   return $.ajax({
-    url: app.host + "/books", // "http://book-json.herokuapp.com/books"
+    url: app.host + "/books",
+      // ^  "http://book-json.herokuapp.com/books"
     method: 'GET',
     // dataType: 'json'
   });
